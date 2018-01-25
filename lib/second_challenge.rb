e54e783cc3266e2dab96c8943d1637f33152e3b2
danl411412 <daan.b.lee@gmail.com>
2018-01-25 04:05:37 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends => []
+      },
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {},
+      :matriach => {},
+      :hero => {},
+      :heroine_friends => []
       }}
 
-  
+
 end
